77c856b66aae45fb12f71690b8c95f676628857e
FrozenEye <frozen.eye.min@gmail.com>
2024-11-28 23:01:07 +0200

    fix: added main task

--- src/main.c
+++ src/main.c
@@ -1,9 +1,28 @@
+// This project uses the CMake build system.
+#include <pico/stdlib.h>
+
+// add the FreeRTOS headers
 #include "FreeRTOS.h"
 #include "FreeRTOSConfig.h"
 #include "task.h"
 
-void main() {
+/**
+ * @brief Main task function that runs indefinitely.
+ *
+ * @param pvParameters Pointer to the parameters passed to the task (not used).
+ */
+void main_task(void *pvParameters) {
   while (true) {
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    vTaskDelay(pdMS_TO_TICKS(1000));
   }
+}
+
+/**
+ * @brief Entry point of the program.
+ *
+ * This function creates the main task and starts the FreeRTOS scheduler.
+ */
+void main() {
+  xTaskCreate(main_task, "main_task", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY, NULL);
+  vTaskStartScheduler();
 }
